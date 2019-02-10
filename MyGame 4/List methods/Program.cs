982793376334c1c3422ab8a4ad4990e7a5ca9191
karakulpwnz <43544493+karakulpwnz@users.noreply.github.com>
2019-02-11 01:46:20 +0300

    C# 2 lesson-4
    Коллекцию пуль в игру добавлял сам (слишком поздно решил прочесть методичку и не увидел, что там есть готовый код), поэтому код не самым лучшим образом написан.
    Не успел сделать 3 задание и часть 2-ого.

---
+++ MyGame 4/List methods/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_methods
+{
+
+    class Program
+    {
+        public static List<object> list;
+        public static Random rnd;
+        //public static List<int> list_2;
+
+
+        static void Main(string[] args)
+        {
+            list = new List<object>();
+            rnd = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+                list.Add(rnd.Next(0, 10));
+                Console.WriteLine(list.Last());
+            }
+
+            Console.WriteLine(list.Count);
+
+            //list_2 = new List<int>();
+            //list_2.AddRange(list);
+
+            List<object> unique = new List<object>();
+            bool duplicate = new bool();
+
+            int n = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                duplicate = false;
+                for (int j = 0; j < unique.Count; j++)
+                {
+                    duplicate = Equals(list[i], unique[j]);
+                    if (duplicate == true) break;
+                }
+                if (duplicate == false)
+                {
+                    unique.Add(list[i]);
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        if (Equals(list[i],list[j])) n++;
+                    }
+                    Console.WriteLine("Число: " + list[i] + "      Колчество: " + n);
+                    n = 0;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MyGame 4/MyGame/ICollision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyGame
+{
+    /// <summary>
+    /// Интерфейс столкновений
+    /// </summary>
+    interface ICollision
+    {
+        bool Collision(ICollision obj);
+        Rectangle Rect { get; }
+    }
+}
